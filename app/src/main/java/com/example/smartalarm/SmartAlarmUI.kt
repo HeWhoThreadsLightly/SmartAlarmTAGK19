cdf8ab381a836fa2c925db201840d435b9e7d083
HeWhoThreadsLightly <victor.alander@gmail.com>
2023-02-28 16:11:41 +0100

    Made a prototype that allows the order of events to be rearranged.

--- app/src/main/java/com/example/smartalarm/SmartAlarmUI.kt
+++ app/src/main/java/com/example/smartalarm/SmartAlarmUI.kt
@@ -1,13 +1,23 @@
 package com.example.smartalarm
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import org.burnoutcrew.reorderable.ReorderableItem
+import org.burnoutcrew.reorderable.detectReorderAfterLongPress
+import org.burnoutcrew.reorderable.rememberReorderableLazyListState
+import org.burnoutcrew.reorderable.reorderable
 
 class SmartAlarmUI {
 
@@ -23,6 +33,38 @@ fun renderFilter(filter: SmartAlarmFilter){
         textRegex = it
         filter.filter = it.text
     })
+}
+
+@Composable
+fun VerticalReorderList() {
+    val data = remember { mutableStateOf(List(100) { "$it" }) }
+    val state = rememberReorderableLazyListState(onMove = { from, to ->
+        data.value = data.value.toMutableList().apply {
+            add(to.index, removeAt(from.index))
+        }
+    })
+    LazyColumn(
+        state = state.listState,
+        modifier = Modifier
+            .reorderable(state)
+            .detectReorderAfterLongPress(state)
+    ) {
+        item(){
+
+        }
+        items(data.value) { item ->
+            ReorderableItem(state, key = item) { isDragging ->
+                val elevation = animateDpAsState(if (isDragging) 16.dp else 0.dp)
+                Column(
+                    modifier = Modifier
+                        .shadow(elevation.value)
+                        .background(MaterialTheme.colorScheme.surface)
+                ) {
+                    Text(item)
+                }
+            }
+        }
+    }
 }
 
 @Composable
@@ -46,33 +88,58 @@ fun renderAlarm(){
     var alarm = SmartAlarmAlarm(5, "Example")
     var textName by remember { mutableStateOf(TextFieldValue(alarm.name)) }
     var textStart by remember { mutableStateOf(TextFieldValue("90")) }
-    
+
+    val data = remember { mutableStateOf(List(100) { "$it" }) }
+    val state = rememberReorderableLazyListState(onMove = { from, to ->
+        data.value = data.value.toMutableList().apply {
+            add(to.index, removeAt(from.index))
+        }
+    })
+
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ){
-        Column(
+        LazyColumn(
+            state = state.listState,
             modifier = Modifier
-                .verticalScroll(rememberScrollState())
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.error, RectangleShape)
-
+                .reorderable(state)
+                .detectReorderAfterLongPress(state)
         ) {
-            TextField(value = textName, modifier = Modifier.fillMaxSize(), onValueChange = {
-                textName = it
-                alarm.name = it.text
-            })
-            Text(text = "Start")
-            TextField(value = textStart, modifier = Modifier.fillMaxWidth(), onValueChange = {textStart = it})
-            Text(text = "Event in calendar")
-            Text(text = "Filters")
-            alarm.filters.forEach{
-                renderFilter(filter = it)
-            }
+            item(){
+                Column(
+                    modifier = Modifier
+                        //.verticalScroll(rememberScrollState())
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.error, RectangleShape)
 
-            Text(text = "Alarm sequence")
-            alarm.events.forEach{
-                renderEvent(event = it)
+                ) {
+                    TextField(value = textName, modifier = Modifier.fillMaxSize(), onValueChange = {
+                        textName = it
+                        alarm.name = it.text
+                    })
+                    Text(text = "Start")
+                    TextField(value = textStart, modifier = Modifier.fillMaxWidth(), onValueChange = {textStart = it})
+                    Text(text = "Event in calendar")
+                    Text(text = "Filters")
+                    alarm.filters.forEach{
+                        renderFilter(filter = it)
+                    }
+
+                    Text(text = "Alarm sequence")
+                }
+            }
+            items(data.value) { item ->
+                ReorderableItem(state, key = item) { isDragging ->
+                    val elevation = animateDpAsState(if (isDragging) 16.dp else 0.dp)
+                    Column(
+                        modifier = Modifier
+                            .shadow(elevation.value)
+                            .background(MaterialTheme.colorScheme.surface)
+                    ) {
+                        renderEvent(event = SmartAlarmEvent(item))
+                    }
+                }
             }
         }
     }
