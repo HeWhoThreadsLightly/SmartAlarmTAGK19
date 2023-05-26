fc74b3575b323ed785b1dfc6593ea4dd4e60552d
reeshy <64738767+reeshy@users.noreply.github.com>
2023-05-26 15:03:49 +0200

    Improved filter padding
    Improved filter padding, fixed issue #14

--- app/src/main/java/com/example/smartalarm/graphics/RenderFilter.kt
+++ app/src/main/java/com/example/smartalarm/graphics/RenderFilter.kt
@@ -1,25 +1,53 @@
 package com.example.smartalarm.graphics
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import com.example.smartalarm.model.SmartAlarmFilter
 
 @Composable
-fun renderFilter(filter: SmartAlarmFilter){
+fun renderFilter(filter: SmartAlarmFilter) {
     var textRegex by remember { mutableStateOf(TextFieldValue(filter.filter)) }
-    Text(text = filter.filterType.name)
+    Text(
+        text = filter.filterType.name,
+        modifier = Modifier
+            .padding(start = 10.dp, top = 0.dp, bottom = 0.dp) // Add padding to the left side
+    )
 
-    if(filter.active){
-        TextField(
-            value = textRegex,
-            onValueChange = {
-                textRegex = it
-                filter.filter = it.text
-            }
-        )
+    if (filter.active) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .border(1.dp, Color.Black)
+                .fillMaxWidth()
+        ) {
+            TextField(
+                value = textRegex,
+                onValueChange = {
+                    textRegex = it
+                    filter.filter = it.text
+                },
+                modifier = Modifier.fillMaxWidth() // Add this modifier
+            )
+        }
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
 
