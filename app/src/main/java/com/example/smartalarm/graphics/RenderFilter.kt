89fa0a67e77df3af1bce151d632fd671ebcb2b88
reeshy <64738767+reeshy@users.noreply.github.com>
2023-05-26 15:43:20 +0200

    Added toggel so filter can be inactivated
    Add toggel so filter can be inactivated issue #10 fixed

--- app/src/main/java/com/example/smartalarm/graphics/RenderFilter.kt
+++ app/src/main/java/com/example/smartalarm/graphics/RenderFilter.kt
@@ -3,9 +3,11 @@ package com.example.smartalarm.graphics
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
@@ -15,39 +17,43 @@ import com.example.smartalarm.model.SmartAlarmFilter
 @Composable
 fun renderFilter(filter: SmartAlarmFilter) {
     var textRegex by remember { mutableStateOf(TextFieldValue(filter.filter)) }
-    Text(
-        text = filter.filterType.name,
-        modifier = Modifier
-            .padding(start = 10.dp, top = 0.dp, bottom = 0.dp) // Add padding to the left side
-    )
+    var isActive by remember { mutableStateOf(filter.active) }
 
-    if (filter.active) {
-        Row(
-            modifier = Modifier
-                .padding(8.dp)
-                .border(1.dp, Color.Black)
-                .fillMaxWidth()
-        ) {
-            TextField(
-                value = textRegex,
-                onValueChange = {
-                    textRegex = it
-                    filter.filter = it.text
-                },
-                modifier = Modifier.fillMaxWidth() // Add this modifier
+    Box(
+        modifier = Modifier
+            .padding(8.dp)
+            .border(1.dp, Color.Black)
+    ) {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                text = filter.filterType.name,
+                modifier = Modifier.padding(start = 8.dp)
             )
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                if (isActive) {
+                    TextField(
+                        value = textRegex,
+                        onValueChange = {
+                            textRegex = it
+                            filter.filter = it.text
+                        },
+                        modifier = Modifier.padding(start = 8.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                Switch(
+                    checked = isActive,
+                    onCheckedChange = {
+                        isActive = it
+                        filter.active = it
+                    },
+                    modifier = Modifier.padding(end = 8.dp)
+                )
+            }
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
 
